356207b645dd981f23c1e11c8555991937a58dbf
Joey Chinoski <joeychin01@yahoo.com>
2019-07-23 09:09:52 -0700

    Set up app.yaml and main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#main.py
+# the import section
+import webapp2
+import jinja2
+import os
+from models import Meme
+from google.appengine.api import urlfetch
+import json
+import random
+random.seed()
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class HomePage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        home_template = the_jinja_env.get_template('templates/home.html')
+        self.response.write(result_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', HomePage), #this maps the root url to the Main Page Handler
+], debug=True)
