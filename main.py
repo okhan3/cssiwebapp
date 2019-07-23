678e026df130814b690949b8eb6cedb575b29155
= <iabouara@gmail.com>
2019-07-23 08:57:30 -0700

    Finished basic post method for InputPage - displays song/artist/lyrics if successfully fetched from api, otherwise displays nothing

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+API_KEY = "VHl5WJoexxSgghXe4zUdqAjrfSfOdbZjCGb6BkrODi5qquF3MFPGzNrFQr1Zsj4W"
+
 class HomePage(webapp2.RequestHandler):
     def get(self): #for a get request
         home_template = the_jinja_env.get_template('/templates/home.html')
@@ -21,13 +23,23 @@ class InputPage(webapp2.RequestHandler):
         input_template = the_jinja_env.get_template('/templates/inputlyrics.html')
         self.response.write(input_template.render())
     def post(self):
-        api_url = "https://orion.apiseeds.com/api/music/"
+        api_url = "https://orion.apiseeds.com/api/music/lyric/"
         api_url += self.request.get("artist_name") + "/"
         api_url += self.request.get("song_name")
-        api_url += "?apikey=VHl5WJoexxSgghXe4zUdqAjrfSfOdbZjCGb6BkrODi5qquF3MFPGzNrFQr1Zsj4W"
+        api_url += "?apikey=" + API_KEY
         apiseeds_response = urlfetch.fetch(api_url)
-        print(apiseeds_response)
-
+        status = apiseeds_response.status_code
+        song = {
+            "status" : status
+        }
+        if int(status) == 200:
+            apiseeds_responseJson = json.loads(apiseeds_response.content)
+            result = apiseeds_responseJson['result']
+            song['artist'] = result['artist']['name']
+            song['track'] = result['track']['name']
+            song['lyrics'] = result['track']['text']
+        input_template = the_jinja_env.get_template('/templates/inputlyrics.html')
+        self.response.write(input_template.render(song))
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
